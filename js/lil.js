9f566439173a5f21dcc7b61351e71ca4eb6097fa
John Earnest <john@beyondloom.com>
2023-09-20 08:24:51 -0700

    fix non-integral offsets to image.translate[], per #62.

--- js/lil.js
+++ js/lil.js
@@ -1415,7 +1415,7 @@ image_make=size=>{
 			shx(-Math.tan(n/2)),shy(Math.sin(n)),shx(-Math.tan(n/2));return self
 		})
 		if(ikey(i,'translate'))return lmnat(([x,y])=>{
-			const o=getpair(x), w=y?lb(y):0;if(o.x==0&&o.y==0)return self;const s=self.size,t=image_make(s)
+			const o=rint(getpair(x)), w=y?lb(y):0;if(o.x==0&&o.y==0)return self;const s=self.size,t=image_make(s)
 			if(w){for(let y=0,z=0;y<s.y;y++)for(let x=0;x<s.x;x++,z++)                           t.pix[z]=self.pix[mod(x-o.x,s.x)+mod(y-o.y,s.y)*s.x]}
 			else {for(let y=0,z=0;y<s.y;y++)for(let x=0;x<s.x;x++,z++){const i=rect(x-o.x,y-o.y);t.pix[z]=(i.x<0||i.x>=s.x||i.y<0||i.y>=s.y)?0: self.pix[i.x+i.y*s.x]}}
 			self.pix.set(t.pix);return self
