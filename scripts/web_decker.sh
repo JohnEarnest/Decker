74dbeb0927abed50bed79f2a8545f3a5a48be132
John Earnest <john@beyondloom.com>
2024-07-13 08:52:51 -0700

    refinements to aid in staging a copy of The Forbidden Library with danger mode enabled.

--- scripts/web_decker.sh
+++ scripts/web_decker.sh
@@ -6,6 +6,7 @@ set -e
 SRC=${1?"Missing source deck"}
 DST=${2?"Missing destination"}
 VERSION=${3?"Missing version number"}
+DANGER=${4:-0}
 
 echo -e "<body><script language=\"decker\">\n" > $DST
 cat $SRC >> $DST
@@ -13,7 +14,7 @@ echo -e "</script>\n" >> $DST
 cat js/decker.html >> $DST
 echo -e "<script>\n" >> $DST
 echo -e "VERSION=\"${VERSION}\"\n" >> $DST
-echo -e "DANGEROUS=0\n" >> $DST
+echo -e "DANGEROUS=${DANGER}\n" >> $DST
 cat js/lil.js >> $DST
 cat js/danger.js >> $DST
 cat js/decker.js >> $DST
