f0e0fdd50230590dfb6404038455849335f5b925
John Earnest <john@beyondloom.com>
2024-07-25 08:31:33 -0700

    logic for script stopping wasn't quite right; we want to switch to widget mode even if we aren't stuck in a modal loop.

--- c/decker.c
+++ c/decker.c
@@ -3472,7 +3472,7 @@ void all_menus(void){
 		menu_bar("Script",1);
 		if(menu_item("Stop",1,'\0')){
 			msg.pending_halt=1;
-			if(ms.type!=modal_none&&ms.type!=modal_query&&ms.type!=modal_listen)modal_exit(0),setmode(mode_object);
+			if(ms.type!=modal_query&&ms.type!=modal_listen){if(ms.type!=modal_none){modal_exit(0);}setmode(mode_object);}
 		}
 		menu_bar("Edit",(ms.type==modal_input||ms.type==modal_save)&&wid.fv);
 		text_edit_menu();
