25b82dffabb6417b0ae64a149000479afe30f04d
John Earnest <john@beyondloom.com>
2024-08-09 15:07:44 -0700

    use history.replaceState() instead of window.location.hash= to avoid adding spurious browser history entries while traversing decks in web-decker.

--- js/decker.js
+++ js/decker.js
@@ -2160,7 +2160,10 @@ go_notify=(deck,x,t,url,delay)=>{
 	if(url&&/^(http|https|ftp|gopher|gemini):\/\//.test(url)){modal_enter('url'),ms.text=fieldstr(lms(url));return}
 	const moved=x!=ln(ifield(ifield(deck,'card'),'index'))
 	if(moved){con_set(null);if(uimode=='script')close_script()}
-	if(moved&&!deck.locked&&x>=0){try{window.location.hash=ls(ifield(deck.cards.v[x],'name'))}catch(e){}}
+	if(moved&&!deck.locked&&x>=0){try{
+		const name=ls(ifield(deck.cards.v[x],'name'))
+		history.replaceState(undefined,undefined,'#'+name)
+	}catch(e){}}
 	const tfun=t==null?null: lion(t)?t: dget(deck.transit,t)
 	if(ms.type!='trans'&&x>=0&&tfun){
 		modal_enter('trans'),ms.time_curr=0,ms.time_end=delay==null?30:0|max(1,ln(delay)),ms.time_start=-1;
