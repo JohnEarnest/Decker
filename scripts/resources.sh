8259e8053af189da69c473a541170554fafc25bb
rofl0r <rofl0r@users.noreply.github.com>
2025-04-10 20:25:28 +0000

    resources.sh: make POSIX sh compatible

--- scripts/resources.sh
+++ scripts/resources.sh
@@ -1,13 +1,18 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 # bundle the HTML/JS of the web-decker implementation as a c header file,
 # for inclusion with the native versions of decker and lilt.
 
 set -e
 
-DECK=${1?"Missing source deck"}
+DECK=$1
+if test -z "$DECK" ; then
+	echo "Missing source deck">&2
+	exit 1
+fi
+
 DST=c/resources.h
 
-echo -e "// auto-generated from web-decker source!\n" > $DST
+printf "%s\n" "// auto-generated from web-decker source!" > $DST
 xxd -i js/lil.js      >> $DST
 xxd -i js/danger.js   >> $DST
 xxd -i js/decker.html >> $DST
