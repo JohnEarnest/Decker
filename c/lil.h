51838458bafc7dfcbd70f53ec85eae8ce30ec784
John Earnest <john@beyondloom.com>
2026-09-25 16:10:04 -0700

    correct a typo and improve test coverage of raze

--- c/lil.h
+++ c/lil.h
@@ -623,7 +623,7 @@ monad(l_raze){
 	if(!lil(x))return l_list(x);
 	if(x->c==0)return LNIL;
 	lv*f=l_first(x);
-	if(lis(f)||lin(f)){int a=1;for(int z=1;z<x->c;z++)if(!lis(f)&&!lin(f)){a=0;break;};if(a)return x;}
+	if(lis(f)||lin(f)){int a=1;for(int z=1;z<x->c;z++)if(!lis(x->lv[z])&&!lin(x->lv[z])){a=0;break;};if(a)return x;}
 	if(lit(f)||lid(f)){lv*r=f;for(int z=1;z<x->c;z++)r=l_comma(r,x->lv[z]);return r;}
 	lv*r=lml(0);for(int z=0;z<x->c;z++)r=l_comma(r,x->lv[z]);return r;
 }
